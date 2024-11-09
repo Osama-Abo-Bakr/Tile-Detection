e2081e05275ebcae04e4e45e98a32655a27b55ff
Osama Abo-Bakr Khalifa <osamaoabobakr12@gmail.com>
2024-11-09 18:25:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pdf2image import convert_from_path
-import sys
+import sys, os
 sys.path.append(os.path.abspath('path/to/submodule/FastSAM'))
 from FastSAM.fastsam import FastSAM, FastSAMPrompt
 from func import masks_to_bool, annotate_image
